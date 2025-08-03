c39be8a019a16ca8d14d1831bc3e83fd2c524389
Luis Rivas <luisfrm_@outlook.com>
2025-08-03 16:15:36 -0400

    feat: update subtitle on hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <section className="relative overflow-hidden bg-background h-[70vh] min-h-[500px] lg:h-screen">
+    <section className="relative h-[700px] lg:h-screen">
       <section className='absolute inset-0 w-full h-full'>
         <div className="relative w-full h-full flex">
           <div className="w-1/2 h-full relative">
@@ -30,12 +30,12 @@ export default function Hero() {
         </div>
       </section>
       <section className="sm:px-6 lg:px-8 relative flex items-center justify-center h-full">
-        <div className="mx-auto max-w-2xl text-center bg-background/60  p-10 rounded-md">
+        <div className="mx-auto max-w-2xl text-center bg-background/70 lg:bg-background/60  p-10 rounded-md">
           <h1 className="font-serif text-4xl font-bold tracking-tight text-foreground sm:text-6xl">
             Capturando momentos
           </h1>
           <p className="mt-6 text-lg leading-8 text-foreground/80">
-            Fotografía profesional para tus momentos más importantes. Bodas, eventos, retratos y más.
+            No es la <strong>cámara</strong> quien hace al fotógrafo, es el <strong>fotógrafo</strong> quien hace a la cámara.
           </p>
           <div className="mt-10 flex flex-col md:flex-row items-center justify-center gap-2">
             <Button asChild size="lg">
